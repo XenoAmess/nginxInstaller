76b90d26fa12c555feac2d1b59938e5178efaa70
XenoAmess <912890489@qq.com>
2018-04-19 23:33:17 +0800

    1
    1

--- nginx/autoinstall.sh
+++ nginx/autoinstall.sh
@@ -1,15 +1,17 @@
-mkdir /nginx/environments/openssl
-cd /nginx/environments/openssl
+mkdir /nginx/environments
+cd /nginx/environments
 git clone https://github.com/openssl/openssl.git
 
-mkdir /nginx/environments/zlib
-cd /nginx/environments/zlib
+mkdir /nginx/environments
+cd /nginx/environments
 git clone https://github.com/madler/zlib
 
-mkdir /nginx/temp
-cd /nginx/temp
+mkdir /nginx
+cd /nginx
 git clone https://github.com/nginx/nginx
 cd /nginx
-mv /nginx/temp/* /nginx/
+mv /nginx/nginx/* /nginx/
+rm -rf /nginx/nginx
+
 ./auto/configure --prefix=/nginx --conf-path=/nginx/nginx.conf --with-openssl=/nginx/environments/openssl --with-http_ssl_module --with-pcre=/nginx/environments/pcre-8.42 --with-zlib=/nginx/environments/zlib
 make install
