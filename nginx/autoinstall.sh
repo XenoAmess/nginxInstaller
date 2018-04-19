7882badfc27a54c442aab79140397621a16f025a
XenoAmess <912890489@qq.com>
2018-04-19 23:56:42 +0800

    1
    1

--- nginx/autoinstall.sh
+++ nginx/autoinstall.sh
@@ -10,5 +10,6 @@ cd /nginx
 mv /nginx/nginx/* /nginx/
 rm -rf /nginx/nginx
 
+chmod 777 /nginx/environments/pcre-8.42/configure
 ./auto/configure --prefix=/nginx --conf-path=/nginx/nginx.conf --with-openssl=/nginx/environments/openssl --with-http_ssl_module --with-pcre=/nginx/environments/pcre-8.42 --with-zlib=/nginx/environments/zlib
 make install
