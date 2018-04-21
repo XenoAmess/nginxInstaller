ce7fd68a4f973b919acc03edefee1f2228fd06d2
XenoAmess <912890489@qq.com>
2018-04-21 17:03:33 +0800

    1
    1

--- nginx/autoinstall.sh
+++ nginx/autoinstall.sh
@@ -3,16 +3,13 @@ autoreconf -vfi
 
 cd /nginx/environments
 git clone https://github.com/openssl/openssl.git
-
-cd /nginx/environments
 git clone https://github.com/madler/zlib
-
-cd /nginx
 git clone https://github.com/nginx/nginx
+git clone https://github.com/maxmind/geoip-api-c
 cd /nginx
-mv /nginx/nginx/* /nginx/
-rm -rf /nginx/nginx
+
+cp -r /nginx/environments//nginx/. /nginx/
 
 chmod 777 /nginx/environments/pcre-8.42/configure
-./auto/configure --prefix=/nginx --conf-path=/nginx/nginx.conf --with-openssl=/nginx/environments/openssl --with-http_ssl_module --with-pcre=/nginx/environments/pcre-8.42 --with-zlib=/nginx/environments/zlib
+./auto/configure --prefix=/nginx --conf-path=/nginx/nginx.conf --with-pcre=/nginx/environments/pcre-8.42 --with-zlib=/nginx/environments/zlib --with-http_ssl_module --with-openssl=/nginx/environments/openssl --with-http_geoip_module --with-geoip=/nginx/environments/geoip-api-c
 make install
